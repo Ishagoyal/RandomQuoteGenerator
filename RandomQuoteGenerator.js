a376d4450f133c92030db85ec576adcef1088e5b
Isha Goyal <iamishagoyal@gmail.com>
2017-01-09 02:37:26 +0530

    try

---
+++ RandomQuoteGenerator.js
@@ -0,0 +1,32 @@
+$(document).ready(function (){
+  $('blockquote').hide();
+  $('.load').show();
+  getQuote();
+  
+  $('#btn-quote').click(function(){
+   $('blockquote').hide();
+  $('.load').show();
+  getQuote();
+  });
+});
+
+ function getQuote(){
+  $.ajax({
+    url:'http://api.forismatic.com/api/1.0/?',
+    dataType:'jsonp',
+    data : "method=getQuote&format=jsonp&lang=en&jsonp=?",
+    success: function(response){
+      $('.load').hide();
+      $('blockquote').show();
+      $('#quoteText').html(response.quoteText);
+      
+      if(response.quoteAuthor !== ''){
+        $('#quoteAuthor').html(response.quoteAuthor);
+      }
+      else{
+        $('#quoteAuhtor').html('Unknown')
+      }
+    }
+    
+  });
+};
