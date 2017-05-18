46277fdbffb349682cff10c1c6802b9bf264aee1
Isha Goyal <iamishagoyal@gmail.com>
2017-05-19 01:34:22 +0530

    update in api url

--- RandomQuoteGenerator.js
+++ RandomQuoteGenerator.js
@@ -14,7 +14,7 @@ $(document).ready(function (){
  
   $.ajax({ 
 
-    url:'http://api.forismatic.com/api/1.0/?',
+    url:'https://api.forismatic.com/api/1.0/?',
     dataType:'jsonp',
     data : 'method=getQuote&format=jsonp&lang=en&jsonp=?',
     success: function(response){
